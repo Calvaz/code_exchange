fdbd149e896ca855f4f0410032eef30949ee8167
calvaz <andreacavo92@gmail.com>
2022-06-17 00:19:08 +0200

    feat: added tailwind styling

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,15 +1,19 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 module.exports = {
-  content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
-  ],
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkGrayBlue: "hsl(227, 12%, 61%)",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
