2f9043398b055313539c0bf3526ee5e9fb6fa9e2
calvaz <andreacavo92@gmail.com>
2022-07-13 00:48:18 +0200

    refactor: used oauth2 and tesla to authenticate

--- lib/code_exchange_web/controllers/auth_controller.ex
+++ lib/code_exchange_web/controllers/auth_controller.ex
@@ -1,19 +1,38 @@
 defmodule CodeExchangeWeb.AuthController do
   use CodeExchangeWeb, :controller
 
-  plug Ueberauth
   alias CodeExchange.User
-  alias Ueberauth.Strategy.Helpers
+  alias CodeExchange.Github
 
-  def request(conn, _params) do
-    redirect(conn, to: Routes.live_path(conn, CodeExchangeWeb.HomeLive), callback_url: Helpers.callback_url(conn))
+  def request(conn, _provider) do
+    redirect conn, external: authorize_url!()
   end
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
-    changeset = User.changeset(%User{}, user_params)
+  def callback(conn, %{"error_description" => error_description}) do
+    conn
+    |> put_flash(:error, "Failed to authenticate because #{error_description}")
+    |> redirect(to: Routes.live_path(conn, CodeExchangeWeb.HomeLive))
+  end
 
-    signin(conn, changeset)
+  def callback(conn, %{"provider" => _provider, "code" => code}) do
+    client = get_token!(code)
+
+    # Request the user's data with the access token
+    case Github.get_user_email(client) do
+      {:ok, response} ->
+        for %{primary: true, email: email} <- response.body do
+          changeset = User.changeset(%User{}, %{email: email})
+          signin(conn, changeset)
+        end
+      {:error, error} ->
+        error
+    end
+  end
+
+  def callback(conn, %{"error_description" => error_description}) do
+    conn
+    |> put_flash(:info, "An error occurred: #{error_description}")
+    |> redirect(to: Routes.live_path(conn, CodeExchangeWeb.HomeLive))
   end
 
   defp upsert_user(changeset) do
@@ -31,6 +50,7 @@ defmodule CodeExchangeWeb.AuthController do
         conn
         |> put_flash(:info, "Welcome back")
         |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
         |> redirect(to: Routes.live_path(conn, CodeExchangeWeb.HomeLive))
       {:error, _reason} ->
         conn
@@ -43,5 +63,12 @@ defmodule CodeExchangeWeb.AuthController do
     conn
     |> configure_session(drop: true)
     |> redirect(to: Routes.live_path(conn, CodeExchangeWeb.HomeLive))
+  end
+
+  defp authorize_url!(),   do: Github.authorize_url!
+  defp get_token!(code),   do: Github.get_token!(code: code)
+  defp get_user!(client) do
+    %{body: user} = OAuth2.Client.get!(client, "/user")
+    %{name: user["name"], avatar: user["avatar_url"]}
   end
 end
